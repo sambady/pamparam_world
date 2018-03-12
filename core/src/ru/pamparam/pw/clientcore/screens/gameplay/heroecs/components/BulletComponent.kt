247b99715823d36996f98d94cd01f21ede94f306
sambady <eshim2009@gmail.com>
2018-03-13 00:27:16 +0200

    Add box2d + box2d light + shoot

--- core/src/ru/pamparam/pw/clientcore/screens/gameplay/heroecs/components/BulletComponent.kt
+++ core/src/ru/pamparam/pw/clientcore/screens/gameplay/heroecs/components/BulletComponent.kt
@@ -16,16 +16,17 @@ class BulletComponent : Component
     val texture by lazy { Texture("bullet.png") }
     val body : Body
     var lifetime = 3f
-    val box2dWorld : World
-    constructor(gameplay: GameplayScreen, position : Vector2, rotation : Float) {
-        box2dWorld = gameplay.box2dWorld
+    val gameplay : GameplayScreen
+    val pointLight : PointLight
+    constructor(gameplay_: GameplayScreen, position : Vector2, rotation : Float) {
+        gameplay = gameplay_
         val bodyDef = BodyDef()
 
         bodyDef.type = BodyDef.BodyType.DynamicBody
         bodyDef.position.set(-texture.width.toFloat()/2f, -texture.height.toFloat()/2f)
 
 
-        body = box2dWorld.createBody(bodyDef)
+        body = gameplay.box2dWorld.createBody(bodyDef)
         body.massData.mass = 0.001f
         body.isFixedRotation = true
         val shape = CircleShape()
@@ -34,12 +35,12 @@ class BulletComponent : Component
         body.createFixture(shape, 1f);
         body.setTransform(position.x, position.y, (rotation + 90f)* MathUtils.degreesToRadians)
 
-        val speed = Vector2(10000000000f, 0f).rotate(rotation)
+        val speed = Vector2(10000000000000f, 0f).rotate(rotation)
 
         Log.info("Dir ${speed}")
         body.applyForceToCenter(speed, true)
 
-        val pointLight = PointLight(gameplay.rayHandler, 128, Color(1f, 0f, 0f, 1.0f), 10f, 0f, 0f)
+        pointLight = PointLight(gameplay.rayHandler, 128, Color(1f, 0f, 0f, 1.0f), 10f, 0f, 0f)
         pointLight.setStaticLight(false);
         pointLight.attachToBody(body)
 
@@ -49,7 +50,10 @@ class BulletComponent : Component
     }
 
     fun dispose() {
-        box2dWorld.destroyBody(body)
+
+        gameplay.box2dWorld.destroyBody(body)
+        pointLight.remove(true)
+
         texture.dispose()
     }
 }
